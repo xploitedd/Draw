e345cdb8c979e5e12419e8abc7e6f90522ad0df9
GenericDrugs <hugomdias.st@gmail.com>
2019-05-11 16:27:08 +0100

    Figure View skeleton

--- app/src/main/java/pt/isel/poo/li21d/g6/draw/view/DrawView.java
+++ app/src/main/java/pt/isel/poo/li21d/g6/draw/view/DrawView.java
@@ -32,4 +32,5 @@ public class DrawView extends View {
         //TODO
         return super.onTouchEvent(event);
     }
+
 }
--- app/src/main/java/pt/isel/poo/li21d/g6/draw/view/FigureView.java
+++ app/src/main/java/pt/isel/poo/li21d/g6/draw/view/FigureView.java
@@ -1,4 +1,25 @@
 package pt.isel.poo.li21d.g6.draw.view;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import pt.isel.poo.li21d.g6.draw.model.Figure;
+
 public class FigureView {
+
+
+    protected Paint paint = new Paint();
+
+    FigureView() {
+
+    }
+
+    void draw(Canvas c) {
+        //TODO
+    }
+
+    FigureView newInstance(Figure f) {
+        //TODO
+        return null;
+    }
 }
